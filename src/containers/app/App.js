7894f011d5313489c33c34c2488907c76fed2f4c
lqy <1064839051@qq.com>
2018-04-10 15:05:14 +0800

    打包后文件

--- src/containers/app/App.js
+++ src/containers/app/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect  } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch, Redirect  } from 'react-router-dom';
 import { Breadcrumb } from 'antd';
 import Loadable from 'react-loadable';
 import Menu from './../../components/menu';
